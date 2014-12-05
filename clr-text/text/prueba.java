9f8ee5186665d6ed5de78c06de7dcb7b9dd397f6
Erick <Erick@GA>
2014-12-05 11:45:35 -0700

    Algo modificable

---
+++ clr-text/text/prueba.java
@@ -0,0 +1,19 @@
+package text;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class prueba {
+	
+	public static void main(String[] args){
+		
+		System.out.println("funciona");
+	}
+
+	@Test
+	public void test() {
+		fail("Funcional");
+	}
+
+}
